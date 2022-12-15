8c6945c4d56f3f06fd721678f66c1db906c4c504
MyIDDQD <nickkosh@gmail.com>
2022-12-15 16:43:47 +0100

    Stage 2/5

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -1,5 +1,25 @@
 package tictactoe
 
+const val ROWS = 3
+const val COLUMNS = 2
+const val EMPTY = "_"
+const val CELL_X = "X"
+const val CELL_O = "O"
+
+const val BORDER_HORIZONTAL = "---------"
+const val BORDER_VERTICAL = "|"
+
+
 fun main() {
-    // write your code here
+    printFieldTwo(readln())
+}
+
+fun printFieldTwo(input: String) {
+    var x = input
+    println(BORDER_HORIZONTAL)
+    repeat(ROWS) {
+        print("$BORDER_VERTICAL ${x.substring(0, COLUMNS + 1).toList().joinToString(" ")} $BORDER_VERTICAL\n")
+        x = x.removeRange(0, COLUMNS + 1)
+    }
+    println(BORDER_HORIZONTAL)
 }
